b1d4ec9bb84bfac5b7e58daddec45b6c216cf87d
AnnaLemons <a.lopezdelg@gmail.com>
2021-10-28 09:10:43 +0000

    saved

--- src/js/component/MyNavBar.jsx
+++ src/js/component/MyNavBar.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import PropTypes from "prop-types";
 
 const MyNavBar = props => {
@@ -11,7 +9,7 @@ const MyNavBar = props => {
 		<Navbar bg="dark" variant="dark">
 			<Container fluid>
 				<Navbar.Brand href="#startbootstrap">
-					Start Bootstrap
+					The Snicth
 				</Navbar.Brand>
 				<Nav className="me-right">
 					<Nav.Link href="#home">{props.buttonOne}</Nav.Link>
